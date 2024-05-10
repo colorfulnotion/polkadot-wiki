e311fa1d07faf2a32bbeba848ce0d66c8732f001
agbld <agiblida@gmail.com>
2024-05-10 06:30:42 +0000

    docs: Update sidebars.js to include DuneSQL cheatsheet in Polkadot Wiki

--- polkadot-wiki/sidebars.js
+++ polkadot-wiki/sidebars.js
@@ -128,6 +128,7 @@ module.exports = {
                     // "general/dune-analytics/parachain-dashboards/interlay-dashboards",
                   ],
                 },
+                "general/dune-analytics/dunesql-cheatsheet",
               ],
             },
           ],
